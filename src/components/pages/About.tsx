f154c344e40b77571a1d2a97c9ea6ceab50c3175
amar <ustavdica.private@gmail.com>
2026-01-19 15:59:51 +0100

    this should be now installable and work ofline too

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -17,7 +17,7 @@ export default function About() {
       </div>
 
       {/* Items explaining about app */}
-      <div className="mt-16 flex flex-col gap-8">
+      <div className="mt-16 flex flex-col gap-4">
         <ItemAbout
           icon={
             <IconWrapper>
