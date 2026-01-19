eb97fbd993e170d9939dfe145d21f69054c70884
amar <ustavdica.private@gmail.com>
2026-01-19 14:27:56 +0100

    Structure of verification page

---
+++ src/components/atoms/VerificationOption.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  id: number;
+  option: string;
+}
+
+export default function VerificationOption({ id, option }: Props) {
+  return (
+    <button className="flex items-center gap-4 bg-orange-950/10 rounded-xl p-4 cursor-pointer">
+      <div className="w-4 h-4 bg-orange-950/20 rounded-full"></div>
+      <p>{option}</p>
+    </button>
+  );
+}
