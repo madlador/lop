9ecec771280ff017b3f74fc50a8fd2f4c9ca8d96
amar <ustavdica.private@gmail.com>
2026-01-19 15:25:27 +0100

    some todos added in code

--- src/components/atoms/VerificationOption.tsx
+++ src/components/atoms/VerificationOption.tsx
@@ -5,8 +5,13 @@ interface Props {
 
 export default function VerificationOption({ id, option }: Props) {
   return (
+    // todo: Here this should be remembered when user clicks on it
+    // the parent component should remember it, so we can verify when
+    // user presses confirm button
     <button
-      onClick={() => console.log(id)}
+      onClick={() => {
+        console.log(id);
+      }}
       className="flex items-center gap-4 bg-orange-950/10 rounded-xl p-4 cursor-pointer"
     >
       <div className="w-4 h-4 bg-orange-950/20 rounded-full"></div>
