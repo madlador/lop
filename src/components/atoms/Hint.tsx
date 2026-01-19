1df7baabb751ede2e58d28c82e3f26e0eca01174
Ivan Krstev <ivankrstev246@gmail.com>
2026-01-19 01:20:27 +0100

    refactor: add hint text props to Hint component

--- src/components/atoms/Hint.tsx
+++ src/components/atoms/Hint.tsx
@@ -1,13 +1,14 @@
 import { RiLightbulbLine } from "@remixicon/react";
 
-export default function Hint() {
+type Props = {
+  hint: string;
+};
+
+export default function Hint({ hint }: Props) {
   return (
-    <div className="flex gap-2 px-4 py-2 rounded-xl shadow bg-yellow-600/30 text-yellow-900">
-      <RiLightbulbLine className="shrink-0 mt-0.5" />
-      <p>
-        “You are looking for a religious building, but not a large or dominant
-        one.”
-      </p>
+    <div className='flex gap-2 px-4 py-2 rounded-xl shadow bg-yellow-600/30 text-yellow-900'>
+      <RiLightbulbLine className='shrink-0 mt-0.5' />
+      <p>{hint}</p>
     </div>
   );
 }
