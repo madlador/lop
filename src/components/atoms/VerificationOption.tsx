77499fe558938a683ac3dc78ecce739cacb1f8c0
amar <ustavdica.private@gmail.com>
2026-01-19 14:40:57 +0100

    refalction page structure

--- src/components/atoms/VerificationOption.tsx
+++ src/components/atoms/VerificationOption.tsx
@@ -5,7 +5,10 @@ interface Props {
 
 export default function VerificationOption({ id, option }: Props) {
   return (
-    <button className="flex items-center gap-4 bg-orange-950/10 rounded-xl p-4 cursor-pointer">
+    <button
+      onClick={() => console.log(id)}
+      className="flex items-center gap-4 bg-orange-950/10 rounded-xl p-4 cursor-pointer"
+    >
       <div className="w-4 h-4 bg-orange-950/20 rounded-full"></div>
       <p>{option}</p>
     </button>
